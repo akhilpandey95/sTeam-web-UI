9fd3d50ded9a239afd148790f59d70fcd52fd13e
AkhilHector <akhilhector.1@gmail.com>
2016-06-24 20:26:41 +0530

    Added editor options for fixing the icons issue
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- controllers/router.js
+++ controllers/router.js
@@ -1,31 +1,15 @@
 angular.module('steam',
   [
-    'ui.router',
-    'ui.bootstrap',
-    'LocalStorageModule',
-    'textAngular',
-    'ngPDFViewer',
-    'ngSanitize'
+  'ui.router',
+  'ui.bootstrap',
+  'LocalStorageModule',
+  'textAngular',
+  'ngPDFViewer',
+  'ngSanitize'
   ])
 
-  .config(['$locationProvider', '$stateProvider', '$urlRouterProvider', '$provide',
-    function ($locationProvider, $stateProvider, $urlRouterProvider, $provide) {
-
-    /*
-    $provide.decorator('editorOptions', ['$delegate', function ($delegate, editorOptions) {
-      editorOptions.bold.iconclass = "fa fa bold";
-      editorOptions.italics.iconclass = "fa fa italic";
-      editorOptions.underline.iconclass = "fa fa underline";
-      editorOptions.ul.iconclass = "fa fa list";
-      editorOptions.ol.iconclass = "fa fa list-ol";
-      editorOptions.undo.iconclass = "fa fa undo";
-      editorOptions.redo.iconclass = "fa fa repeat";
-      editorOptions.justifyLeft.iconclass = "fa fa-align-left";
-      editorOptions.justifyCenter.iconclass = "fa fa-align-justify";
-      editorOptions.justifyRight.iconclass = "fa fa-align-right";
-      return $delegate;
-    }])
-    */
+.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', '$provide',
+  function ($locationProvider, $stateProvider, $urlRouterProvider, $provide) {
 
     $stateProvider.state('login', {
       url: '/login',
@@ -34,77 +18,95 @@ angular.module('steam',
       requireLogin: false
     })
 
-      .state('register', {
-        url: '/register',
-        templateUrl: '/templates/login.html',
-        controller: 'registerCtrl',
-        requireLogin: false
-      })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/templates/login.html',
+      controller: 'registerCtrl',
+      requireLogin: false
+    })
 
-      .state('workarea', {
-        url: '/',
-        requireLogin: true,
-        templateUrl: '/templates/workarea.html',
-        params: {
-          autoActivateChild: 'workarea.list'
-        }
-      })
+    .state('workarea', {
+      url: '/',
+      requireLogin: true,
+      templateUrl: '/templates/workarea.html',
+      params: {
+        autoActivateChild: 'workarea.list'
+      }
+    })
 
-      .state('workarea.groups', {
-        url: '^/groups/:path',
-        requireLogin: true,
-        views: {
-          'options': {
-            templateUrl: '/views/options.html',
-            controller: 'optionsCtrl'
-          },
-          'groupsList': {
-            templateUrl: '/views/groupsList.html',
-            controller: 'groupsListCtrl'
-          }
+    .state('workarea.groups', {
+      url: '^/groups/:path',
+      requireLogin: true,
+      views: {
+        'options': {
+          templateUrl: '/views/options.html',
+          controller: 'optionsCtrl'
+        },
+        'groupsList': {
+          templateUrl: '/views/groupsList.html',
+          controller: 'groupsListCtrl'
         }
-      })
+      }
+    })
 
-      .state('workarea.list', {
-        url: '^/room/:path',
-        requireLogin: true,
-        views: {
-          'options': {
-            templateUrl: '/views/options.html',
-            controller: 'optionsCtrl'
-          },
-          'workspaceList': {
-            templateUrl: '/views/workspaceList.html',
-            controller: 'workspaceListCtrl'
-          },
-          'comments': {
-            templateUrl: '/views/comments.html',
-            controller: 'commentsCtrl'
-          }
+    .state('workarea.list', {
+      url: '^/room/:path',
+      requireLogin: true,
+      views: {
+        'options': {
+          templateUrl: '/views/options.html',
+          controller: 'optionsCtrl'
+        },
+        'workspaceList': {
+          templateUrl: '/views/workspaceList.html',
+          controller: 'workspaceListCtrl'
+        },
+        'comments': {
+          templateUrl: '/views/comments.html',
+          controller: 'commentsCtrl'
         }
-      })
+      }
+    })
 
-      .state('workarea.detailed', {
-        url: '^/document/:path',
-        requireLogin: true,
-        views: {
-          'options': {
-            templateUrl: '/views/options.html',
-            controller: 'optionsCtrl'
-          },
-          'workspaceDetailed': {
-            templateUrl: '/views/workspaceDetailed.html',
-            controller: 'workspaceDetailedCtrl'
-          },
-          'editor': {
-            templateUrl: '/views/editor.html',
-            controller: 'workSpaceEditorCtrl'
-          },
-          'comments': {
-            templateUrl: '/views/comments.html',
-            controller: 'commentsCtrl'
-          }
+    .state('workarea.detailed', {
+      url: '^/document/:path',
+      requireLogin: true,
+      views: {
+        'options': {
+          templateUrl: '/views/options.html',
+          controller: 'optionsCtrl'
+        },
+        'workspaceDetailed': {
+          templateUrl: '/views/workspaceDetailed.html',
+          controller: 'workspaceDetailedCtrl'
+        },
+        'editor': {
+          templateUrl: '/views/editor.html',
+          controller: 'workSpaceEditorCtrl'
+        },
+        'comments': {
+          templateUrl: '/views/comments.html',
+          controller: 'commentsCtrl'
         }
-      })
-  $urlRouterProvider.otherwise('/room/')
-}])
+      }
+    })
+    $urlRouterProvider.otherwise('/room/')
+  }])
+
+
+
+.constant('editorConfig', function ($provide) {
+  $provide.decorator('editorOptions', ['$delegate', function ($delegate, editorOptions) {
+    editorOptions.bold.iconclass = "fa fa bold";
+    editorOptions.italics.iconclass = "fa fa italic";
+    editorOptions.underline.iconclass = "fa fa underline";
+    editorOptions.ul.iconclass = "fa fa list";
+    editorOptions.ol.iconclass = "fa fa list-ol";
+    editorOptions.undo.iconclass = "fa fa undo";
+    editorOptions.redo.iconclass = "fa fa repeat";
+    editorOptions.justifyLeft.iconclass = "fa fa-align-left";
+    editorOptions.justifyCenter.iconclass = "fa fa-align-justify";
+    editorOptions.justifyRight.iconclass = "fa fa-align-right";
+    return $delegate;
+  }])
+})
