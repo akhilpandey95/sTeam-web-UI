9c2483178099546d1ba9e53e621601afef9c16a7
AkhilHector <akhilhector.1@gmail.com>
2016-06-28 18:32:19 +0530

    Updated dragObj directive in order to emit drag events
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- directives/dragObj.js
+++ directives/dragObj.js
@@ -1,7 +1,24 @@
  angular.module('steam')
 
- .directive('dragObj', ['$parse', function ($parse) {
+ .directive('dragObj', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'A',
+    link: function (scope, element, attrs, ctrl) {
+      // get the element first
+      angular.element(element).attr("isDraggable": true)
+      var id = angular.element(element).attr("id")
+
+      // emit events
+      element.bind("startDrag", function (data) {
+        console.log("Starting to drag")
+        $rootScope.emit("STEAM-DRAG-STRT")
+      });
+
+      element.bind("stopDrag", function (data) {
+        console.log('Stopping to drag')
+        $rootScope.emit("STEAM-DRAG-STOP")
+      });
+
+    }
   }
 }])
