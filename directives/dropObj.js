25d3302c07538cba3b0d1c973df965e116054bce
AkhilHector <akhilhector.1@gmail.com>
2016-06-29 21:53:11 +0530

    Updated dropObj directive in order to emit drop events
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- directives/dropObj.js
+++ directives/dropObj.js
@@ -1,7 +1,28 @@
  angular.module('steam')
 
- .directive('dropObj', ['$parse', function ($parse) {
-  return {
-    restrict: 'A',
-  }
-}])
+ .directive('dropObj', ['$rootScope', function ($rootScope) {
+ 	return {
+ 		restrict: 'A',
+ 		link: function (scope, element, attrs, ctrl) {
+
+ 			element.bind("dragenter", function (data) {
+ 				angular.element(data.target).addClass('drag-over');
+ 			});
+
+ 			element.bind("dragleave", function (data) {
+ 				angular.element(data.target).removeClass('drag-over');
+ 			});
+
+ 			$rootScope.$on("STEAM-DRAG-STRT", function () {
+ 				var domelm = document.getElementById(id);
+ 				angular.element(domelm).addClass("drag-target");
+ 			});
+
+ 			$rootScope.$on("STEAM-DRAG-STOP", function () {
+ 				var domelm = document.getElementById(id);
+ 				angular.element(domelm).removeClass("drag-target");
+ 				angular.element(domelm).removeClass("drag-over");
+ 			});
+ 		}
+ 	}
+ }])
