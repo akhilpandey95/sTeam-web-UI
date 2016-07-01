329e8db96e05600a1a64c60d303f777a5111b265
AkhilHector <akhilhector.1@gmail.com>
2016-07-01 22:32:27 +0530

    Updated the dropObj directive
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- directives/dropObj.js
+++ directives/dropObj.js
@@ -4,25 +4,56 @@
  	return {
  		restrict: 'A',
  		link: function (scope, element, attrs, ctrl) {
+ 			// create uuid
+		    var uuid = {
+	      	create: function () {
+	      		function _p8(s) {
+	      			var p = (Math.random().toString(16)+"000000000").substr(2,8);
+	      			return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+	      		}
+	      		return _p8() + _p8(true) + _p8(true) + _p8();
+	      	}
+	      }
 
- 			element.bind("dragenter", function (data) {
- 				angular.element(data.target).addClass('drag-over');
- 			});
+		  var id = angular.element(element).attr("id")
+	      if(!id) {
+	      	id = uuid.create()
+	      	angular.element(element).attr("id", id)
+	      }
 
- 			element.bind("dragleave", function (data) {
- 				angular.element(data.target).removeClass('drag-over');
- 			});
+	      element.bind("dragenter", function (data) {
+	      	angular.element(data.target).addClass('drag-over');
+	      });
 
- 			$rootScope.$on("STEAM-DRAG-STRT", function () {
- 				var domelm = document.getElementById(id);
- 				angular.element(domelm).addClass("drag-target");
- 			});
+	      element.bind("dragleave", function (data) {
+	      	angular.element(data.target).removeClass('drag-over');
+	      });
 
- 			$rootScope.$on("STEAM-DRAG-STOP", function () {
- 				var domelm = document.getElementById(id);
- 				angular.element(domelm).removeClass("drag-target");
- 				angular.element(domelm).removeClass("drag-over");
- 			});
- 		}
- 	}
- }])
+	      element.bind("drop", function (e) {
+      		if (e.preventDefault) {
+                e.preventDefault(); // Necessary. Allows us to drop.
+            }
+
+            if (e.stopPropagation) {
+                e.stopPropagation(); // Necessary. Allows us to drop.
+            }
+            var data = e.originalEvent.dataTransfer.getData("text");
+            var dest = document.getElementById(id);
+            var src = document.getElementById(data);
+
+			 scope.onDrop({dragEl: data, dropEl: id});
+          });
+
+	      $rootScope.$on("STEAM-DRAG-STRT", function () {
+	      	var domelm = document.getElementById(id);
+	      	angular.element(domelm).addClass("drag-target");
+	      });
+
+	      $rootScope.$on("STEAM-DRAG-STOP", function () {
+	      	var domelm = document.getElementById(id);
+	      	angular.element(domelm).removeClass("drag-target");
+	      	angular.element(domelm).removeClass("drag-over");
+	      });
+		  }
+		}
+	}])
