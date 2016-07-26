c289dacd610b3cdf569b533d11e6383d7148fe69
AkhilHector <akhilhector.1@gmail.com>
2016-07-26 14:33:25 +0530

    Added second component to the interface, the audio player
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- controllers/workspaceCtrl.js
+++ controllers/workspaceCtrl.js
@@ -1,7 +1,7 @@
 angular.module('steam')
 
-  .controller('workspaceListCtrl', ['$rootScope', '$scope', 'handler', 'localStorageService',
-    function ($rootScope, $scope, handler, localStorageService) {
+.controller('workspaceListCtrl', ['$rootScope', '$scope', 'handler', 'localStorageService',
+  function ($rootScope, $scope, handler, localStorageService) {
     $scope.invokeObj = function (itemClass, itemPath, itemMimeType) {
       while(itemPath.charAt(0) === '/') {
         itemPath = itemPath.substr(1)
@@ -24,27 +24,27 @@ angular.module('steam')
     }
   }])
 
-  .controller('workspaceDetailedCtrl', ['$http', '$scope', 'handler', 'localStorageService', 'PDFViewerService', '$sce',
-   function ($http, $scope, handler, localStorageService, pdf, $sce) {
-    $scope.dataSrc = localStorageService.get('baseurl') + 'home/' + localStorageService.get('currentObjPath')
+.controller('workspaceDetailedCtrl', ['$http', '$scope', 'handler', 'localStorageService', 'PDFViewerService', '$sce',
+ function ($http, $scope, handler, localStorageService, pdf, $sce) {
+  $scope.dataSrc = localStorageService.get('baseurl') + 'home/' + localStorageService.get('currentObjPath')
 
-    $scope.mimeTypeHandler = function () {
-      if(localStorageService.get('currentObjMimeType') == 'application/x-unknown-content-type') {
-        return 'unknown'
-      } else if (localStorageService.get('currentObjMimeType').match(/image\/*/)) {
-        return 'image'
-      } else if (localStorageService.get('currentObjMimeType') == 'application/pdf') {
-        return 'pdf'
-      } else if (localStorageService.get('currentObjMimeType').match(/audio\/*/)) {
-        return 'audio'
-      } else if (localStorageService.get('currentObjMimeType').match(/video\/*/)) {
-        return 'video'
-      } else if (localStorageService.get('currentObjMimeType').match(/text\/*/) ||
-        localStorageService.get('currentObjMimeType') == 'application/x-javascript' ||
-        localStorageService.get('currentObjMimeType') == 'application/x-pike') {
-          return 'text'
-      } else { return 'notfound' }
-    }
+  $scope.mimeTypeHandler = function () {
+    if(localStorageService.get('currentObjMimeType') == 'application/x-unknown-content-type') {
+      return 'unknown'
+    } else if (localStorageService.get('currentObjMimeType').match(/image\/*/)) {
+      return 'image'
+    } else if (localStorageService.get('currentObjMimeType') == 'application/pdf') {
+      return 'pdf'
+    } else if (localStorageService.get('currentObjMimeType').match(/audio\/*/)) {
+      return 'audio'
+    } else if (localStorageService.get('currentObjMimeType').match(/video\/*/)) {
+      return 'video'
+    } else if (localStorageService.get('currentObjMimeType').match(/text\/*/) ||
+      localStorageService.get('currentObjMimeType') == 'application/x-javascript' ||
+      localStorageService.get('currentObjMimeType') == 'application/x-pike') {
+      return 'text'
+    } else { return 'notfound' }
+  }
 
     // Trusting the resource
     $scope.source = $sce.trustAsResourceUrl($scope.dataSrc)
@@ -77,67 +77,67 @@ angular.module('steam')
     }
   }])
 
-  .controller('workSpaceEditorCtrl', ['$scope', 'handler', 'localStorageService', 'textAngularManager', '$document',
-   function ($scope, handler, localStorageService, textAngularManager, $document) {
+.controller('workSpaceEditorCtrl', ['$scope', 'handler', 'localStorageService', 'textAngularManager', '$document',
+ function ($scope, handler, localStorageService, textAngularManager, $document) {
+  $scope.data = {
+    empty: '<small>Please enter text</small>',
+    full: ''
+  }
+  $scope.editable = true;
+  $scope.content = $scope.data.empty;
+
+  $scope.allh1 = function() {
+    textAngularManager.updateToolDisplay('h1', {
+      buttontext: 'Heading 1'
+    });
+  }
+
+  $scope.allh2 = function() {
+    textAngularManager.updateToolDisplay('h2', {
+      buttontext: 'Heading 2'
+    });
+  }
+
+  $scope.allh3 = function() {
+    textAngularManager.updateToolDisplay('h3', {
+      buttontext: 'Heading 3'
+    });
+  }
+
+  $scope.allh4 = function() {
+    textAngularManager.updateToolDisplay('h4', {
+      buttontext: 'Heading 4'
+    });
+  }
+
+  $scope.allh5 = function() {
+    textAngularManager.updateToolDisplay('h5', {
+      buttontext: 'Heading 5'
+    });
+  }
+
+  $scope.allh6 = function() {
+    textAngularManager.updateToolDisplay('h6', {
+      buttontext: 'Heading 6'
+    });
+  }
+
+  $scope.submit = function () {
+    console.log("The document has been submitted");
+  }
+  $scope.clear = function () {
+    console.log("The document has been reset");
     $scope.data = {
-      empty: '<small>Please enter text</small>',
-      full: ''
+      orightml: $scope.content
     }
-    $scope.editable = true;
-    $scope.content = $scope.data.empty;
+  }
+  $scope.resetEditor = function () {
+    textAngularManager.resetToolsDisplay();
+  }
+}])
 
-    $scope.allh1 = function() {
-      textAngularManager.updateToolDisplay('h1', {
-        buttontext: 'Heading 1'
-      });
-    }
-
-    $scope.allh2 = function() {
-      textAngularManager.updateToolDisplay('h2', {
-        buttontext: 'Heading 2'
-      });
-    }
-
-    $scope.allh3 = function() {
-      textAngularManager.updateToolDisplay('h3', {
-        buttontext: 'Heading 3'
-      });
-    }
-
-    $scope.allh4 = function() {
-      textAngularManager.updateToolDisplay('h4', {
-        buttontext: 'Heading 4'
-      });
-    }
-
-    $scope.allh5 = function() {
-      textAngularManager.updateToolDisplay('h5', {
-        buttontext: 'Heading 5'
-      });
-    }
-
-    $scope.allh6 = function() {
-      textAngularManager.updateToolDisplay('h6', {
-        buttontext: 'Heading 6'
-      });
-    }
-
-    $scope.submit = function () {
-      console.log("The document has been submitted");
-    }
-    $scope.clear = function () {
-      console.log("The document has been reset");
-      $scope.data = {
-        orightml: $scope.content
-      }
-    }
-    $scope.resetEditor = function () {
-      textAngularManager.resetToolsDisplay();
-    }
-  }])
-
-    .controller('workSpaceImgviewerCtrl', ['$scope', 'handler', 'localStorageService', 'Lightbox', '$sce',
-   function ($scope, handler, localStorageService, Lightbox, $sce) {
+.controller('workSpaceImgviewerCtrl', ['$scope', 'handler', 'localStorageService', 'Lightbox', '$sce',
+ function ($scope, handler, localStorageService, Lightbox, $sce) {
 
     //imagee url
     $scope.imgSrc = localStorageService.get('baseurl') + 'home/' + localStorageService.get('currentObjPath')
@@ -157,15 +157,26 @@ angular.module('steam')
 
   }])
 
-    .controller('workSpaceMediaPlayerCtrl', ['$scope', 'handler', 'localStorageService', 'ngAudio', '$sce',
-   function ($scope, handler, localStorageService, ngAudio, $sce) {
+.value("workspaceAudio",{})
+.controller('workspaceAudioResourceCtrl', function($scope, ngAudio) {
+  $scope.audios = [
+  ngAudio.load('audio/test.mp3'),
+  ]
+})
 
-    //media url
-    $scope.mediaSrc = localStorageService.get('baseurl') + 'home/' + localStorageService.get('currentObjPath')
+.controller('workspaceAudioCtrl', ['$scope', 'ngAudio', 'workspaceAudio', 'localStorageService', '$sce',
+  function($scope, ngAudio, workspaceAudio, localStorageService, $sce) {
 
-    //trust the source
-    $scope.source = $sce.trustAsResourceUrl($scope.mediaSrc)
+    $scope.dataSrc = localStorageService.get('baseurl') + 'home/' + localStorageService.get('currentObjPath')
+    $scope.source = $sce.trustAsResourceUrl($scope.dataSrc)
+    var url = $scope.dataSrc;
 
-    $scope.audio = ngAudio.load($scope.mediaSrc);
-
-  }])
+    if (workspaceAudio[url]) {
+      $scope.audio = workspaceAudio[url];
+    }
+    else {
+      $scope.audio = ngAudio.load(url);
+      $scope.audio.volume = 0.8;
+      workspaceAudio[url] = $scope.audio;
+    }
+}])
